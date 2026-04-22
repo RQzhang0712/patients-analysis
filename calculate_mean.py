468f70b1f3e23656b9edf4c53d3779d5b4403d57
RuqianZ <lxcc53067@outlook.com>
2026-04-22 15:02:26 +0200

    add usage

--- calculate_mean.py
+++ calculate_mean.py
@@ -3,6 +3,10 @@ This function computes the mean of an array
 Input: A one dimensional array
 Output: The mean of the array
 """
+# usage:
+# data = np.array([0, 10, 20, 30])
+# mean = calculate_mean(data)
+
 
 import numpy as np
 
